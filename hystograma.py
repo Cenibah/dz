ae4c2a893b7f9e9a4ff9bad750c2284ab10fd3cc
Cenibah <cenibah@ukr.net>
2015-12-09 16:38:15 +0200

    Add file polindr and hystogram

---
+++ hystograma.py
@@ -0,0 +1,5 @@
+hist = [5, 8, 11, 1, 4]
+
+for i in hist:
+    print ('*' * i)
+    
---
+++ polindr.py
@@ -0,0 +1,6 @@
+def is_palindrome(word):
+    return word == word[::-1]
+
+print(is_palindrome('abba'))
+print(is_palindrome('Led Zeppelin'))
+#print(is_palindrome('Eva, can I stab bats in a cave'))
